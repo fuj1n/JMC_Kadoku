ad58c06731bb452a443b06d82fd79d06946ff24e
Arthur Uzulin <fuj1nman@hotmail.com>
2018-11-25 03:43:25 +1100

    Sidebar styling, DOTween and dragging components

---
+++ Bullet Hack/Assets/Scripts/UI/CodeBlockDrag.cs
@@ -0,0 +1,50 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CodeBlockDrag : MonoBehaviour, IDragHandler, IPointerEnterHandler, IPointerExitHandler, IEndDragHandler
+{
+    public Color outlineHover;
+    public float fadeTime = .5F;
+
+    private RectTransform rect;
+    private Outline outline;
+
+    private CanvasScaler scaler;
+
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+
+        outline = GetComponent<Outline>();
+        outline.DOFade(0F, 0F);
+
+        scaler = GetComponentInParent<CanvasScaler>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rect.anchoredPosition += eventData.delta / scaler.transform.lossyScale;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (!eventData.hovered.Contains(gameObject))
+            outline.DOFade(0F, fadeTime);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        outline.effectColor = outlineHover;
+        outline.DOFade(1F, fadeTime);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.dragging && eventData.pointerDrag == gameObject)
+            return;
+
+        outline.DOFade(0F, fadeTime);
+    }
+}
---
+++ Bullet Hack/Assets/Scripts/UI/ScrollableArea.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ScrollableArea : MonoBehaviour, IDragHandler
+{
+    public RectTransform content;
+    public Button centerButton;
+
+    private CanvasScaler scaler;
+
+    private void Awake()
+    {
+        scaler = GetComponentInParent<CanvasScaler>();
+
+        if (centerButton)
+            centerButton.onClick.AddListener(() => content.anchoredPosition = Vector2.zero);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        content.anchoredPosition += eventData.delta / scaler.transform.localScale.x;
+        //content.anchoredPosition = new Vector2(content.anchoredPosition.x, content.anchoredPosition.y);
+    }
+}
