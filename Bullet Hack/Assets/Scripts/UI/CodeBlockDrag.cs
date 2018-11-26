d3d9496ee06958a6c7037e222268d941e2733e8f
Arthur Uzulin <fuj1nman@hotmail.com>
2018-11-26 19:23:54 +1100

    Block creation and deletion

--- Bullet Hack/Assets/Scripts/UI/CodeBlockDrag.cs
+++ Bullet Hack/Assets/Scripts/UI/CodeBlockDrag.cs
@@ -1,15 +1,25 @@
 ﻿using DG.Tweening;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class CodeBlockDrag : MonoBehaviour, IDragHandler, IPointerEnterHandler, IPointerExitHandler, IEndDragHandler
+public class CodeBlockDrag : MonoBehaviour, IDragHandler, IPointerEnterHandler, IPointerExitHandler, IBeginDragHandler, IEndDragHandler
 {
+    public bool cloneDrag;
+    [ConditionalHide(true, ConditionalSourceField = "cloneDrag")]
+    public RectTransform cloneTarget;
+
+    public bool undeletable;
+
     public Color outlineHover;
+    public Color outlineDelete;
     public float fadeTime = .5F;
 
     private RectTransform rect;
     private Outline outline;
+
+    private EventSystem system;
 
     private void Awake()
     {
@@ -17,16 +27,40 @@ public class CodeBlockDrag : MonoBehaviour, IDragHandler, IPointerEnterHandler, 
 
         outline = GetComponent<Outline>();
         outline.DOFade(0F, 0F);
+
+        system = FindObjectOfType<EventSystem>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (cloneDrag && cloneTarget)
+        {
+            Instantiate(gameObject, transform.parent).name = gameObject.name;
+            transform.SetParent(cloneTarget, true);
+            transform.localScale = Vector3.one;
+            system.SetSelectedGameObject(null, eventData);
+            cloneDrag = false;
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
+        if(!undeletable && eventData.hovered.Any(x => x.CompareTag("UI-Bin")))
+        {
+            outline.DOColor(outlineDelete, fadeTime);
+        } else
+        {
+            outline.DOColor(outlineHover, fadeTime);
+        }
+
         rect.anchoredPosition += eventData.delta / rect.parent.lossyScale;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        if (!eventData.hovered.Contains(gameObject))
+        if (!undeletable && eventData.hovered.Any(x => x.CompareTag("UI-Bin")))
+            Destroy(gameObject);
+        else if (!eventData.hovered.Contains(gameObject))
             outline.DOFade(0F, fadeTime);
     }
 
