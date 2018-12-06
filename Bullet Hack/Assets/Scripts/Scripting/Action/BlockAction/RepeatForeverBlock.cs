0dcac35ecdb26d78e537d4055a1746c635f26dc7
Arthur Uzulin <fuj1nman@hotmail.com>
2018-12-07 01:26:11 +1100

    Added repeat forever block

---
+++ Bullet Hack/Assets/Scripts/Scripting/Action/BlockAction/RepeatForeverBlock.cs
@@ -0,0 +1,12 @@
+﻿[BlockLoader.Block(BlockLoader.BlockAttribute.BlockType.BRACKET)]
+public class RepeatForeverBlock : ActionBlockBase
+{
+    public override string GetName() => "Repeat Forever";
+
+    public override void Next() { }
+
+    public override bool ShouldExecute()
+    {
+        return true;
+    }
+}
