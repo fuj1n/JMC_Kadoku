d997284e2851a2e99a6e34198a4354cdee0f3b2b
Arthur Uzulin <fuj1nman@hotmail.com>
2019-03-07 02:41:24 +1100

    Open world player animation

--- Bullet Hack/Assets/Scripts/BulletHack/World/PlayerController.cs
+++ Bullet Hack/Assets/Scripts/BulletHack/World/PlayerController.cs
@@ -4,10 +4,20 @@ namespace BulletHack.World
 {
     public class PlayerController : MonoBehaviour
     {
-        public float rotationDamping = .2F;
         public float movementSpeed = 25F;
+
+        [Header("Animation")]
+        public float tiltAmount = 45F;
+        public float tiltDamping = .25F;
+        public float rotationDamping = .2F;
+
+        [Header("Particles")]
+        public ParticleSystem[] particles;
+        public float particleMinSpeed = 5F;
+        public float particleMaxSpeed = 10F;
         
         private float yDampVelocity;
+        private float xDampVelocity;
 
         private Rigidbody rb;
 
@@ -30,6 +40,15 @@ namespace BulletHack.World
                 Vector3 newRot = transform.eulerAngles.Set(angle, Utility.Axis.Y);
                 
                 transform.eulerAngles = newRot;
+            }
+            
+            float tilt = Mathf.SmoothDampAngle(transform.eulerAngles.x, tiltAmount * movement.magnitude, ref xDampVelocity, tiltDamping);
+            transform.eulerAngles = transform.eulerAngles.Set(tilt, Utility.Axis.X);
+
+            foreach (ParticleSystem particle in particles)
+            {
+                ParticleSystem.MainModule main = particle.main;
+                main.startSpeed = Mathf.Lerp(particleMinSpeed, particleMaxSpeed, movement.magnitude);
             }
 
             rb.velocity = movement * movementSpeed;
