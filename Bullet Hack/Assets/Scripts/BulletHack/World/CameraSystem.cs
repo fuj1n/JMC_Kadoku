0ac154b4cb6828b659679e6c10d97ee414f75e96
Arthur Uzulin <fuj1nman@hotmail.com>
2019-03-07 01:12:06 +1100

    Open World Player Movement

---
+++ Bullet Hack/Assets/Scripts/BulletHack/World/CameraSystem.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace BulletHack.World
+{
+    public class CameraSystem : MonoBehaviour
+    {
+        public static CameraSystem Instance { get; private set; }
+        
+        private Camera NullCam
+        {
+            get
+            {
+                if (!nullCam)
+                    nullCam = new Camera {enabled = false, name = "NULL Camera"};
+            
+                Debug.LogWarning("[WARN] No active camera, returning null camera");
+                return nullCam;
+            }
+        }
+        private Camera nullCam;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        [NotNull]
+        public Camera GetActiveCamera()
+        {
+            return Camera.main != null ? Camera.main : NullCam;
+        }
+    }
+}
---
+++ Bullet Hack/Assets/Scripts/BulletHack/World/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace BulletHack.World
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float rotationDamping = .2F;
+        public float movementSpeed = 25F;
+        
+        private float yDampVelocity;
+
+        private Rigidbody rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            Quaternion camera = Quaternion.Euler(CameraSystem.Instance.GetActiveCamera().transform.eulerAngles.Isolate(Utility.Axis.Y));
+            
+            Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0F, Input.GetAxisRaw("Vertical"));
+            movement = camera * movement;
+
+            if (movement.magnitude > Mathf.Epsilon)
+            {
+                float angle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
+                angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref yDampVelocity,  rotationDamping);
+                Vector3 newRot = transform.eulerAngles.Set(angle, Utility.Axis.Y);
+                
+                transform.eulerAngles = newRot;
+            }
+
+            rb.velocity = movement * movementSpeed;
+        }
+    }
+}
