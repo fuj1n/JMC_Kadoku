b28409d4363070a94ac8a3bde4865092df05fcd1
Arthur Uzulin <fuj1nman@hotmail.com>
2019-03-20 19:45:34 +1100

    Texture animator skeleton

---
+++ Bullet Hack/Assets/Scripts/BulletHack/FX/TextureAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace BulletHack.FX
+{
+    public class TextureAnimation : MonoBehaviour
+    {
+        public int fps = 15;
+        public Vector2Int frameCount;
+        public Vector2 textureSize;
+        
+        private float Spf => 1F / fps;
+        
+        private void Update()
+        {
+            
+        }
+    }
+}
