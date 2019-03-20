c9968eba57d90547f24d97c15e435362fda5f2a1
Arthur Uzulin <fuj1nman@hotmail.com>
2019-03-21 03:21:08 +1100

    Texture animator

--- Bullet Hack/Assets/Scripts/BulletHack/FX/TextureAnimation.cs
+++ Bullet Hack/Assets/Scripts/BulletHack/FX/TextureAnimation.cs
@@ -7,12 +7,32 @@ namespace BulletHack.FX
         public int fps = 15;
         public Vector2Int frameCount;
         public Vector2 textureSize;
+
+        private Material mat;
         
         private float Spf => 1F / fps;
         
+        private float nextFrame = 0F;
+        private int frame = 0;
+
+        private void Awake()
+        {
+            mat = GetComponent<Renderer>().material;
+        }
+
         private void Update()
         {
-            
+            nextFrame -= Time.deltaTime;
+            if (nextFrame <= 0)
+            {
+                nextFrame = Spf;
+                
+                mat.mainTextureOffset = new Vector2(Mathf.FloorToInt(frame % frameCount.x), Mathf.FloorToInt((float)frame / frameCount.x)) * textureSize;
+                
+                frame++;
+                if (frame > frameCount.x * frameCount.y)
+                    frame = 0;
+            }
         }
     }
 }
