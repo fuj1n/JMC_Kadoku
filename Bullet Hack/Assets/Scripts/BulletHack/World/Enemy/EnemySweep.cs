fa7135e1231621b9b49ce1114dc938b6ee70e22d
Arthur Uzulin <fuj1nman@hotmail.com>
2019-04-17 07:31:10 +1000

    Finished mission 2 enemy setup

---
+++ Bullet Hack/Assets/Scripts/BulletHack/World/Enemy/EnemySweep.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace BulletHack.World.Enemy
+{
+    public class EnemySweep : MonoBehaviour
+    {
+        public bool continuous;
+        
+        [ConditionalHide(nameof(continuous), true, true)]
+        public float minAngle;
+        [ConditionalHide(nameof(continuous), true, true)]
+        public float maxAngle;
+
+        public float speed = 5F;
+        
+        public int direction = 1;
+        private float angle;
+
+        private void Awake()
+        {
+            angle = direction > 0 ? minAngle : maxAngle;
+        }
+
+        private void Update()
+        {
+            angle += speed * Time.deltaTime * Mathf.Sign(direction);
+
+            if (!continuous && (angle > maxAngle || angle < minAngle))
+            {
+                direction = angle > maxAngle ? -1 : 1;
+                angle = Mathf.Clamp(angle, minAngle, maxAngle);
+            }
+            
+            transform.eulerAngles = transform.eulerAngles.Set(angle, Utility.Axis.Y);
+        }
+    }
+}
